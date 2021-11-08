8cec62f095729e85fb4e121bd2991c4a215d2d29
Junghun Kim <mnthe@bluehole.net>
2021-11-08 10:33:02 +0900

    Update powershell_profile.ps1

--- powershell_profile.ps1
+++ powershell_profile.ps1
@@ -1,3 +1,9 @@
+# Set prompt
 Import-Module posh-git
 Import-Module oh-my-posh
 Set-PoshPrompt -Theme powerlevel10k_lean
+
+# Set kubectl auto completion
+Import-Module PSKubectlCompletion  
+Set-Alias k -Value kubectl  
+Register-KubectlCompletion  
