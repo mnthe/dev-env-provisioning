16ef16824393ea2f94c5bb2be96112f49785a767
Junghun Kim <mnthe@bluehole.net>
2022-05-10 10:02:39 +0900

    Update powershell_profile.ps1

--- powershell_profile.ps1
+++ powershell_profile.ps1
@@ -1,4 +1,5 @@
 # Set prompt
+Import-Module posh-git
 Import-Module oh-my-posh
 oh-my-posh init powershell -c "https://raw.githubusercontent.com/mnthe/dev-env-provisioning/main/.oh-my-posh/themes/default.json" | Invoke-Expression
 
