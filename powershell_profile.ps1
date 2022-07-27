5f17b7882402232ceee3ddce53d08ac33e03d062
Junghun Kim <mnthe@bluehole.net>
2022-07-27 10:24:19 +0900

    Update powershell_profile.ps1

--- powershell_profile.ps1
+++ powershell_profile.ps1
@@ -1,6 +1,5 @@
 # Set prompt
 Import-Module posh-git
-Import-Module oh-my-posh
 oh-my-posh init powershell -c "$HOME\.oh-my-posh\themes\default.json" | Invoke-Expression
 
 # Set kubectl auto completion
