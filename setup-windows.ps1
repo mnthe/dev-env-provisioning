46650b36c3e5a167dbe20fe3f039f88997fdaffd
Junghun Kim <mnthe@bluehole.net>
2023-06-29 16:02:52 +0900

    Update setup-windows.ps1

--- setup-windows.ps1
+++ setup-windows.ps1
@@ -2,13 +2,18 @@
 
 $ProgressPreference = 'SilentlyContinue'
 
-choco install 1password --yes --force
 choco install keybase --yes --force
 choco install googlechrome --yes --force
 choco install steam --yes --force
 choco install slack --yes --force
 choco install discord --yes --force
 choco install powertoys --yes --force
+choco install notion --yes --force
+
+# Install 1password
+$ONE_PASSWORD="$HOME\Downloads\1PasswordSetup-latest.exe"
+Invoke-WebRequest -Uri https://downloads.1password.com/win/1PasswordSetup-latest.exe -OutFile $ONE_PASSWORD
+Start-Process -Wait -FilePath $ONE_PASSWORD -NoNewWindow -PassThru # Need Confirm
 
 # Install Kakaotalk
 $KAKAO_TALK = "$HOME\Downloads\KakaoTalk_Setup.exe"
@@ -20,13 +25,11 @@ choco install git --yes --force
 choco install gnupg --yes --force
 choco install vscode --yes --force
 choco install docker-desktop --yes --force
-choco install visualstudio2022community --yes --force `
-    --package-parameters "--add Microsoft.VisualStudio.Workload.NetWeb --add Microsoft.VisualStudio.Workload.NetCoreTools --includeRecommended --passive --locale en-US"
+choco install visualstudio2022professional --yes --force `
+    --package-parameters "--add Microsoft.VisualStudio.Workload.NetWeb --add Microsoft.VisualStudio.Workload.NetCoreTools --includeRecommended --passive --locale ko-KR"
 choco install nodejs-lts --yes --force
 choco install golang --yes --force
 choco install python --yes --force
-choco install aws-iam-authenticator --yes --force
-choco install lens --yes --force
 choco install kubernetes-cli --yes --force
 choco install postman --yes --force
 choco install vault --yes --force
@@ -41,8 +44,7 @@ choco install firacodenf --yes --force
 # Install AWS CLI2
 msiexec.exe /i https://awscli.amazonaws.com/AWSCLIV2.msi /qn+
 
-# Install ansible
-pip3 install ansible
+# Install teleport
 
 # Config git
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/mnthe/dev-env-provisioning/main/.gitconfig" -OutFile $env:USERPROFILE\.gitconfig
@@ -50,13 +52,12 @@ Invoke-WebRequest -Uri "https://raw.githubusercontent.com/mnthe/dev-env-provisio
 # Config Powershell
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
 Install-PackageProvider -Name NuGet -Force
+Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://ohmyposh.dev/install.ps1')) # Install oh-my-posh
 Install-Module posh-git -Scope CurrentUser -Force
-Install-Module oh-my-posh -Scope CurrentUser -Force
 Install-Module PSKubectlCompletion -Scope CurrentUser -Force
 mkdir -p "$HOME\.oh-my-posh\themes\"
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/mnthe/dev-env-provisioning/main/.oh-my-posh/themes/default.json" -OutFile "$HOME\.oh-my-posh\themes\default.json"
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/mnthe/dev-env-provisioning/main/powershell_profile.ps1" -OutFile $profile
-
 
 # Install WSL2 (https://docs.microsoft.com/ko-kr/windows/wsl/install-win10)
 
@@ -65,12 +66,12 @@ dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux 
 ## 3. Virtual Machine 기능 사용 (https://docs.microsoft.com/ko-kr/windows/wsl/install-manual#step-3---enable-virtual-machine-feature)
 dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
 ## 4. Linux 커널 업데이트 패키지 다운로드 (https://docs.microsoft.com/ko-kr/windows/wsl/install-manual#step-4---download-the-linux-kernel-update-package)
-msiexec.exe /i https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi /qn+
+msiexec.exe /i https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi
 ## 5. WSL 2를 기본 버전으로 설정 (https://docs.microsoft.com/ko-kr/windows/wsl/install-manual#step-5---set-wsl-2-as-your-default-version)
 wsl --set-default-version 2
 ## 6. 선택한 Linux 배포 설치 (https://docs.microsoft.com/ko-kr/windows/wsl/install-manual#step-6---install-your-linux-distribution-of-choice)
 curl.exe -L -o ubuntu.appx https://aka.ms/wslubuntu
-Add-AppxPackage .\app_name.appx
+Add-AppxPackage .\ubuntu.appx 
 
 # Enable Sandbox
 Enable-WindowsOptionalFeature -FeatureName "Containers-DisposableClientVM" -All -Online
